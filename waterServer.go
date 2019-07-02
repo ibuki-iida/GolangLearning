f2ebb3a7845f82e415e76f1142e58b67a5769d1f
ibuki-iida <51314950+ibuki-iida@users.noreply.github.com>
2019-07-02 12:06:03 +0900

    グローバル変数の排除

--- waterServer.go
+++ waterServer.go
@@ -13,19 +13,19 @@ const (
 	waterOutAmountMl  int = 10000 //デフォルト200ml テストの為10000ml
 )
 
-var (
+type waterAmount struct {
 	currentBottoleAmountMl int
 	currentHotAmountMl     int
 	currentColdAmountMl    int
-)
+}
 
 //水の準備　フィールドにある変数に水を入れる
-func prepareWater() {
-	changeBottle()
-	currentBottoleAmountMl -= currentHotAmountMl
-	currentHotAmountMl += maxHotCapacityMl
-	currentBottoleAmountMl -= currentColdAmountMl
-	currentColdAmountMl += maxColdCapacityMl
+func prepareWater(wa *waterAmount) {
+	changeBottle(wa)
+	wa.currentBottoleAmountMl -= wa.currentHotAmountMl
+	wa.currentHotAmountMl += maxHotCapacityMl
+	wa.currentBottoleAmountMl -= wa.currentColdAmountMl
+	wa.currentColdAmountMl += maxColdCapacityMl
 
 }
 
@@ -35,7 +35,7 @@ func scanWithRestrictions(useStrings ...string) (string, bool) {
 	scanner := bufio.NewScanner(os.Stdin) //入力待ちの処理
 	scanner.Scan()
 	var inputStr string
-	var isError bool = false
+	isError := false
 	inputStr = scanner.Text()
 
 	for _, useStr := range useStrings {
@@ -50,25 +50,25 @@ func scanWithRestrictions(useStrings ...string) (string, bool) {
 }
 
 //ボトルを交換する
-func changeBottle() {
-	currentBottoleAmountMl = bottleCapacityMl
+func changeBottle(wa *waterAmount) {
+	wa.currentBottoleAmountMl = bottleCapacityMl
 }
 
 //水を出す
-func drainWater(waterTemperatur string) {
+func drainWater(waterTemperatur string, wa *waterAmount) {
 	switch waterTemperatur {
 	case "1":
-		currentColdAmountMl -= waterOutAmountMl
-		currentBottoleAmountMl -= waterOutAmountMl
+		wa.currentColdAmountMl -= waterOutAmountMl
+		wa.currentBottoleAmountMl -= waterOutAmountMl
 	case "2":
-		currentHotAmountMl -= waterOutAmountMl
-		currentBottoleAmountMl -= waterOutAmountMl
+		wa.currentHotAmountMl -= waterOutAmountMl
+		wa.currentBottoleAmountMl -= waterOutAmountMl
 	}
 }
 
 //水量チェック　水が空だったら補充を促す
-func checkWaterAmount() {
-	if currentBottoleAmountMl <= 0 {
+func checkWaterAmount(wa *waterAmount) {
+	if wa.currentBottoleAmountMl <= 0 {
 		for {
 			fmt.Println("水を補充して下さい。")
 			fmt.Println("補充が完了したら「１」を入力して下さい。")
@@ -78,21 +78,22 @@ func checkWaterAmount() {
 				break
 			}
 		}
-		changeBottle()
+		changeBottle(wa)
 	}
 }
 
 //水量の表示
-func printWatereAmount() {
-	fmt.Printf("ボトルの残量は%vmlです。\n", currentBottoleAmountMl)
+func printWatereAmount(wa *waterAmount) {
+	fmt.Printf("ボトルの残量は%vmlです。\n", wa.currentBottoleAmountMl)
 }
 
 func main() {
+	var wa = new(waterAmount)
 	//水を準備
-	prepareWater()
+	prepareWater(wa)
 	for {
-		checkWaterAmount()
-		printWatereAmount()
+		checkWaterAmount(wa)
+		printWatereAmount(wa)
 		fmt.Println("冷たい水の場合は[1]を、熱いお湯の場合は[2]を入力して下さい。")
 		rawInputStr := []string{"1", "2"}
 		inputStr, isError := scanWithRestrictions(rawInputStr...)
@@ -101,7 +102,7 @@ func main() {
 		}
 
 		//水を出す
-		drainWater(inputStr)
+		drainWater(inputStr, wa)
 
 	}
 }
